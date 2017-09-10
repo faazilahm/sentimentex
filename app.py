3b78e80841466bd41c29dcf7a5803357afdcc4d2
faazilahm <31786809+faazilahm@users.noreply.github.com>
2017-09-10 06:49:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,7 @@ from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.py')
-pages = FlatPages(app)
-freezer = Freezer(app)
+
  
 mp  =ml_script_pennapps.ModelPrediction()
 
